b3f50539966538c4f556f33ba69c127f1fc87410
apurbahasanj <apurbahasanj@gmail.com>
2024-03-27 03:31:56 +0600

    website setup process section done

---
+++ src/components/SetupProcess.jsx
@@ -0,0 +1,21 @@
+const SetupProcess = () => {
+  return (
+    <section className="container text-center mt-20">
+      <h3>
+        ওয়েবসাইট <span className="text-primary">সেটাপ প্রসেস</span>
+      </h3>
+      <img
+        className="lg:block hidden"
+        src="https://storex.com.bd/imgs/pages/storexInfoGraphicsWeb.png"
+        alt="how to set Template"
+      />
+      <img
+        className="block lg:hidden mx-auto mt-10"
+        src="https://storex.com.bd/imgs/pages/storexInfoGraphicsMobile.png"
+        alt="how to set Template"
+      />
+    </section>
+  );
+};
+
+export default SetupProcess;
--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -45,7 +45,7 @@ const features = [
 
 const WhyUs = () => {
   return (
-    <section className="container h-screen mt-20">
+    <section className="container mt-20">
       <div className=" border-orange-500 border-l-8 rounded-l-md pl-3 py-1">
         <p className="text-secondary text-lg">Take a look</p>
         <h4 className="mt-2">
