3c29343747296dd7de554797597da79b2c4ef688
apurbahasanj <apurbahasanj@gmail.com>
2024-03-27 03:31:56 +0600

    website setup process section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // import Hero from '@/components/Hero'
 import Navbar from "@/components/Navbar";
+import SetupProcess from "@/components/SetupProcess";
 import ShortFeatures from "@/components/ShortFeatures";
 import Test from "@/components/Test";
 import WhyUs from "@/components/WhyUs";
@@ -12,6 +13,7 @@ const Home = () => {
       <ShortFeatures />
       {/* <Hero/> */}
       <WhyUs />
+      <SetupProcess/>
     </div>
   );
 };
